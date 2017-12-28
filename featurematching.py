960f1d28f74d5e96aa4acaf192104b0393ca6bcf
prateek951 <prince.cenation@gmail.com>
2017-12-28 20:01:49 +0530

    feature matching algorithm

---
+++ featurematching.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np 
+
+# This is not like template matching wherein the transformations are preserved
+# and where the template image was a spliced portion of the source image
+
+# In this algorithm we have two images one is the source image
+# SOURCE IMAGE - with different lightnening,different angle of rotations
+# TEMPLATE IMAGE -- same applies for this image as well
+
