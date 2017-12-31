fbd6dcdfaa90f7226b693371785ab557cb959795
prateek951 <prince.cenation@gmail.com>
2017-12-31 19:32:40 +0530

    steps to make your own haar cascade

---
+++ makehaarcascade.py
@@ -0,0 +1,19 @@
+# How to make your own HAAR CASCADE FOR IMAGE AND VIDEO OBJECT CLASSIFICATION
+
+# STEPS
+
+# Step - 1 "Collect Negative OR Background Images"
+# Any image will do, just make sure your object is not present in them!
+# Get thousands of such images
+
+# Step - 2 "Collect or create positive image"
+# Thousands of images of your object. Can make these based on one object
+# OR create them manually
+
+# Step - 3 "Create a positive vector by stitching together all the positives"
+# This is done using OpenCV
+
+# Step - 4 "Train Cascade"
+# Done with OpenCV
+
+
