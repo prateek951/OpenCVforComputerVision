91e28e9f6a9f1387cae099a3847a015339cf698e
prateek951 <prince.cenation@gmail.com>
2017-12-31 20:05:49 +0530

    how to add legend and labels

--- matplotlib/lineplot.py
+++ matplotlib/lineplot.py
@@ -3,12 +3,20 @@ from matplotlib import pyplot as plt
 # Function to plot a line 
 
 x = [1,2,3]
-y = [1,2,3]
-plt.plot(x,y)
+y = [5,7,4]
+
+x2 = [1,2,3]
+y2 = [10,14,12]
+
+plt.plot(x,y,label='First Line')
+plt.plot(x2,y2,label='Second Line')
 # Legends,titles and plots in matplotlib
 # which is quite similiar to matlab
 plt.xlabel('Plot number')
 plt.ylabel('Important variable')
-plt.title('Interesting Graph')
+plt.title('Interesting Graph \n Check it out')
+
+# Invoke the legend
+plt.legend()
 
 plt.show()
