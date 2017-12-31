7381ab48214c11ef640f8be602bb931aeefe240d
prateek951 <prince.cenation@gmail.com>
2017-12-31 18:22:16 +0530

    algorithm for bgredn.

---
+++ bgReduction.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np 
+# Using the mog background reduction algorithm
+
+# This algorithm works by finding the changes the ones that 
+# are changing is the foreground and the one which is not changing 
+# is the background as compared from the previus frames
+
