ee68a807ae167d0334b89d6f34aa339eb9707d8a
prateek951 <prince.cenation@gmail.com>
2017-12-31 18:55:28 +0530

    deleting junk

---
+++ haarcascade.py
@@ -0,0 +1,9 @@
+# This algorithm is used to detect the object in the a video using the haar cascade 
+# object detection algorithm
+
+import cv2
+import numpy as np 
+
+# Haar cascades are massive xml files with lot of feature sets.These feature
+# sets might correspond to a specific type of object
+
