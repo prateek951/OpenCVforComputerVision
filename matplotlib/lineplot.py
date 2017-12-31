ac109f65e49f1fa6ec4c4553dd88b42c665fa112
prateek951 <prince.cenation@gmail.com>
2017-12-31 20:02:49 +0530

    added matplotlib

--- matplotlib/lineplot.py
+++ matplotlib/lineplot.py
@@ -1,6 +1,14 @@
 from matplotlib import pyplot as plt 
 
 # Function to plot a line 
-plt.plot([1,2,3],[4,5,6])
+
+x = [1,2,3]
+y = [1,2,3]
+plt.plot(x,y)
+# Legends,titles and plots in matplotlib
+# which is quite similiar to matlab
+plt.xlabel('Plot number')
+plt.ylabel('Important variable')
+plt.title('Interesting Graph')
 
 plt.show()
