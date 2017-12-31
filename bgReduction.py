a2bab83625cb506f244d7380077b1bcca59d6900
prateek951 <prince.cenation@gmail.com>
2017-12-31 18:40:48 +0530

    relocated

--- bgReduction.py
+++ bgReduction.py
@@ -6,3 +6,26 @@ import numpy as np
 # are changing is the foreground and the one which is not changing 
 # is the background as compared from the previus frames
 
+# Capture the video scene
+cap = cv2.VideoCapture('bgrSample.mp4')
+# Createa an instance for the background reduction
+fgbg = cv2.createBackgroundSubtractorMOG2()
+while True:
+    # Read a frame
+    ret,frame = cap.read()
+    # Apply the bg reduction mask on the frame
+    fgmask = fgbg.apply(frame)
+
+    # Display the original frame and the background-reduced version of it
+    cv2.imshow('original', frame)
+    cv2.imshow('fg', fgmask)
+
+    # Waiting for the process to end 
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+# Release the capture
+cap.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
+    
