d32463a6d89cbc89792cd20806f3a1f851d2d9e1
prateek951 <prince.cenation@gmail.com>
2018-01-01 15:11:04 +0530

    bg fg subtration algo

---
+++ bgfgsubtration.py
@@ -0,0 +1,23 @@
+# Gaussina Mixed based Background/Foreground Segmentation Algorithm
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('walking.avi')
+
+# Initlaize background subtractor
+foreground_background = cv2.BackgroundSubtractorMOG()
+
+while True:
+    
+    ret, frame = cap.read()
+
+    # Apply background subtractor to get our foreground mask
+    foreground_mask = foreground_background.apply(frame)
+
+    cv2.imshow('Output', foreground_mask)
+    if cv2.waitKey(1) == 13: 
+        break
+
+cap.release()
+cv2.destroyAllWindows()
